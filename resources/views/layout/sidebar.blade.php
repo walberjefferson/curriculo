afe0dcd44b703ef109097833a06c44972128b7e2
Walber Jefferson <walberjefferson@hotmail.com>
2021-10-14 09:49:19 -0300

    Exclusão de arquivos desnecessários

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -119,32 +119,5 @@
         <a href="#" class="settings-sidebar-toggler">
             <i data-feather="settings"></i>
         </a>
-        <h6 class="text-muted">Sidebar:</h6>
-        <div class="form-group border-bottom">
-            <div class="form-check form-check-inline">
-                <label class="form-check-label">
-                    <input type="radio" class="form-check-input" name="sidebarThemeSettings" id="sidebarLight"
-                           value="sidebar-light" checked>
-                    Light
-                </label>
-            </div>
-            <div class="form-check form-check-inline">
-                <label class="form-check-label">
-                    <input type="radio" class="form-check-input" name="sidebarThemeSettings" id="sidebarDark"
-                           value="sidebar-dark">
-                    Dark
-                </label>
-            </div>
-        </div>
-        <div class="theme-wrapper">
-            <h6 class="text-muted mb-2">Light Version:</h6>
-            <a class="theme-item active" href="https://www.nobleui.com/laravel/template/light/">
-                <img src="{{ url('assets/images/screenshots/light.jpg') }}" alt="light version">
-            </a>
-            <h6 class="text-muted mb-2">Dark Version:</h6>
-            <a class="theme-item" href="https://www.nobleui.com/laravel/template/dark">
-                <img src="{{ url('assets/images/screenshots/dark.jpg') }}" alt="light version">
-            </a>
-        </div>
     </div>
 </nav>
